95853ed970eee6a8bb311344921e8def209a663e
jtinianow <jermtee@gmail.com>
2019-07-21 17:13:52 -0600

    improve responsiveness

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,10 +10,12 @@ const IndexPage = () => (
     <div className="container">
       <h1 className="title">Introducing WestUrb</h1>
       <p className="color-text-light">As the world grows more urban, local governments will play increasingly crucial roles in combatting climate change and providing triple-bottom-line results for their communities. They will do so with high-quality programs in four areas:</p>
-      <Highlight title="Sustainability" text="Ensuring availability of affordable basic resource both today and tomorrow" />
-      <Highlight title="Resilience" text="Reducing the risk of disasters, and bouncing back quickly when they occur" />
-      <Highlight title="Restoration" text="Restoring natural systems to their historic health" />
-      <Highlight title="Regeneration" text="Creating local ecosystems that regenerate amenities naturally" />
+      <section className="highlights">
+        <Highlight title="Sustainability" text="Ensuring availability of affordable basic resource both today and tomorrow" />
+        <Highlight title="Resilience" text="Reducing the risk of disasters, and bouncing back quickly when they occur" />
+        <Highlight title="Restoration" text="Restoring natural systems to their historic health" />
+        <Highlight title="Regeneration" text="Creating local ecosystems that regenerate amenities naturally" />
+      </section>
       <p className="color-text-light">Western Urban Sustainability Advisors, LLC (WestUrb) helps local governments provide such high-quality programs more easily and effectively. WestUrb also helps businesses that want to sell related services and products to local governments, and interest groups that want to influence the adoption of supportive policies by such governments.</p>
     </div>
   </Layout>
