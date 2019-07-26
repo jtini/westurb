2fa95b353d59aa7d489f55cf987206188708c6e9
jtinianow <jermtee@gmail.com>
2019-07-25 19:13:25 -0600

    add logo to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,14 @@ import Layout from "../components/Layout/Layout"
 import SEO from "../components/SEO/SEO"
 import Highlight from "../components/Highlight/Highlight"
 import NavigationList from "../components/NavigationList/NavigationList"
+import logo from '../images/logo-large.svg'
+import './index.scss'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Introducing WestUrb" />
     <div className="container">
+      <img className="homepage-logo" src={logo} width={96} alt="WestUrb logo" />
       <h1 className="title">Introducing WestUrb</h1>
       <p className="color-text-light">As the world grows more urban, local governments will play increasingly crucial roles in combatting climate change and providing triple-bottom-line results for their communities. They will do so with high-quality programs in four areas:</p>
       <section className="highlights">
